d440fcb6d218e598f79d685a95fc4950c70f8577
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2026-02-14 08:00:02 +0000

    add husky and lint-staged 1

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "no-console": "warn",
+    },
+  },
+  {
+    ignores: ["dist", "node_modules", "eslint.config.mjs"],
+  },
+)
